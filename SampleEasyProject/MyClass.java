f480d2efee126b35439b17a46d5814f2eae46071
zacharymendler <zdmkirby000@gmail.com>
2025-02-04 16:38:17 -0800

    Add files via upload

--- SampleEasyProject/MyClass.java
+++ SampleEasyProject/MyClass.java
@@ -1,7 +1,15 @@
-
+import java.util.Scanner;
 public class MyClass
 {
-    //write a for loop that will output the numbers 1 through 100
+     //write a for loop that will output the numbers 1 through 100
     //by 5s, each number separated by a comma
     //example 5,10,15......95,100
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    for(int i=1;i<num;i++){
+        System.out.print(i+",");
+    }
+    System.out.print(num);
+    }
 }
